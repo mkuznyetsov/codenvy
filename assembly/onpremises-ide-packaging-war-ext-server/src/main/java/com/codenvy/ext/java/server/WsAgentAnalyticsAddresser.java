03300337b42534098283187f56d49a6e3a172e6d
Mihail Kuznyetsov <mkuznyetsov@codenvy.com>
2016-11-22 11:06:32 +0200

    Add analytics addressers

---
+++ assembly/onpremises-ide-packaging-war-ext-server/src/main/java/com/codenvy/ext/java/server/WsAgentAnalyticsAddresser.java
@@ -0,0 +1,35 @@
+package com.codenvy.ext.java.server;
+
+import org.eclipse.che.commons.schedule.ScheduleRate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Singleton;
+import javax.net.ssl.HttpsURLConnection;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Mihail Kuznyetsov
+ */
+@Singleton
+public class WsAgentAnalyticsAddresser {
+    private static final Logger LOG = LoggerFactory.getLogger(WsAgentAnalyticsAddresser.class);
+
+    @ScheduleRate(period = 1, unit = TimeUnit.HOURS)
+    void send() {
+        HttpURLConnection connection = null;
+        try {
+            final URL url = new URL("https://install.codenvycorp.com/codenvy/init/workspace");
+            connection = (HttpsURLConnection)url.openConnection();
+            connection.getResponseCode();
+        } catch (Exception e) {
+            LOG.debug("Failed to send agent analytics", e);
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+}
